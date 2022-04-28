3df64b77c73eeb6b4d698e5c6b4f4ea1f4dc536b
ruveydatriesstuff <63712077+ruveydatriesstuff@users.noreply.github.com>
2022-04-28 15:07:35 +0300

    Add files via upload

---
+++ webpage.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import generator as mcrgen
+
+
+def onopen():
+    mcrgen.globalize()
+
+
+def generate():
+    st.code(mcrgen.main(), language="none")
+
+
+onopen()
+st.header("MCR Lyrics Generator")
+st.text("Welcome to a work in progress for our Computational Linguistics class.")
+gen_button = st.button("Generate")
+
+if gen_button:
+    generate()
+else:
+    st.text("Press the Generate button above to get started. ")
+
+# st.button("Generate", on_click=st.text_area("", value=generator.main(), disabled=True))
