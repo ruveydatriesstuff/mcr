8618b220071b9e46643710690ab8ffa9edd2211d
ruveydatriesstuff <63712077+ruveydatriesstuff@users.noreply.github.com>
2022-05-23 10:19:02 +0300

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -13,9 +13,8 @@ def generate():
 onopen()
 st.header("My Computational Romance")
 st.caption("Created to fulfill the requirements of LING360 Computational Linguistics at Boğaziçi University. \n")
-st.text("\n A My Chemical Romance lyrics generator using n-grams. \n")
 gen_button = st.button("Generate")
-
+st.text("\n A My Chemical Romance lyrics generator using n-grams. \n")
 if gen_button:
     generate()
 else:
